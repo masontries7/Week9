6e98f468a0791b6218b990d0c001d3427fafa949
masontries7 <145887619+masontries7@users.noreply.github.com>
2023-12-09 15:15:53 -0500

    Add files via upload

---
+++ UnitTestWk9.js
@@ -0,0 +1,31 @@
+
+function Card(value, suit) {
+    this.value = value;
+    this.suit = suit;
+}
+
+var deck = {
+    cards: new Array(52),
+
+    shuffle: function() {
+
+    },
+
+    deal: function() {
+this.player1 = player1
+this.player2 = player2
+const player1 =! 26
+const player2 =! 26
+
+    }
+}
+console.log(Card);
+
+
+
+for (let i = 1; i < 26; i++){
+    cards.push(Card(i, 'Spade'));
+    cards.push(Card(i, 'Diamond'));
+    cards.push(Card(i, 'Clubs'));
+    cards.push(Card(i, 'Hearts'));
+}
---
+++ UnitTestWk9_test.js
@@ -0,0 +1,57 @@
+//const { expect } = require("chai");
+var expect = chai.expect;
+
+let originalDeck = new CreateDeck();
+
+describe('CreateDeck()', function() {
+    describe('check for type array', function () {
+        it('should return object type array', function() {
+            let deck = new CreateDeck();
+            expect(deck).to.be.a('array');
+        })
+    describe('verify that the deck is shuffled', function() {
+        it('should not be in the same order as the original deck', function() {
+            let deck = new CreateDeck();
+            expect(deck).to.not.equal(originalDeck);
+        })
+    })
+    })
+    // NOTE - showing how these work
+    describe('verify deck contains cards with proper properties', function() {
+        it('should contain the property "suit"', function() {
+            let deck = new CreateDeck();
+            expect(deck[0]).to.have.property('suit');
+        })
+        it('suit should be one of four strings', function() {
+            let deck = new CreateDeck();
+            expect(deck[0].suit).to.be.oneOf(['Hearts', 'Diamonds', 'Spades', 'Clubs']);
+        })
+        it('should contain the property "rank"', function() {
+            let deck = new CreateDeck();
+            expect(deck[0]).to.have.property('rank');
+        })
+        it('should contain the property "value"', function() {
+            let deck = new CreateDeck();
+            expect(deck[0]).to.have.property('value');
+        })
+        
+    })
+
+    describe('verify deck size', function (){
+        it("should be a length of 52", function() {
+            let deck = new CreateDeck;
+            expect(deck.length).to.equal(52);
+        })
+    })
+})
+
+describe('Player Decks', function () {
+    describe('verify deck length for each player deck', function() {
+        it("should be a length of 26 for player1Deck", function() {
+            expect(player1Deck.length).to.equal(26);
+        })
+        it('should be a length of 26 for player2Deck', function() {
+            expect(player2Deck.length).to.equal(26); 
+        })
+    })
+})
